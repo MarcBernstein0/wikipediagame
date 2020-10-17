002d8a5f93f767f6ff3e339e8c7c00021b96e647
Marc Bernstein <marcbernstein98@gmail.com>
2020-10-17 18:36:23 -0400

    Update main
        -- cleaned up main and added tests to specific test functions
    
    [Ticket: X]

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/MarcBernstein0/wikipediagame/services"
 )
 
 func main() {
 	// fmt.Println(links)
-	services.GetLinks("https://en.wikipedia.org/wiki/Go_(programming_language)")
+	links, err := services.GetLinks("https://en.wikipedia.org/wiki/Go_(programming_language)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(links)
 }
