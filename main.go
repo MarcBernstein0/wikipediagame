da1c173d368fa38ef20da38260b36f7cbe303ae3
Marc Bernstein <marcbernstein98@gmail.com>
2020-10-17 14:39:17 -0400

    Testing git login

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/MarcBernstein0/wikipediagame/services"
+import (
+	"fmt"
+
+	"github.com/MarcBernstein0/wikipediagame/services"
+)
 
 func main() {
-	// fmt.Println(links)
+	fmt.Println(links)
 	services.GetLinks("https://en.wikipedia.org/wiki/Go_(programming_language)")
 }
