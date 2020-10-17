69931aa5c7053b3feca1b098dc0322e5bca98187
Marc Bernstein <marcbernstein98@gmail.com>
2020-10-17 14:40:20 -0400

    Removing links print in main

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/MarcBernstein0/wikipediagame/services"
 )
 
 func main() {
-	fmt.Println(links)
+	// fmt.Println(links)
 	services.GetLinks("https://en.wikipedia.org/wiki/Go_(programming_language)")
 }
